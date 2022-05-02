2ce12ea067fbd56b8b33f991dd9ce8e29c2a9dda
Declan Davis <dexthefish@gmail.com>
2022-05-02 19:05:38 -0400

    Added a function to assert (in)equality and played with emojis

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual,expected) {
+  if (actual === expected) {
+    console.log("🍀🍀🍀 Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("😡😡😡 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+/* TESTS
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual('a', 'a');
+assertEqual(0, 0);
+assertEqual(2, 1.1);
+assertEqual('1', 1);
+*/
