50569a39347030f7810a330f6b8ffdf524590299
Declan Davis <dexthefish@gmail.com>
2022-05-03 22:23:35 -0400

    added a function to check equality of arrays

---
+++ eqArrays.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual,expected) {
+  if (actual === expected) {
+    console.log(`🍀🍀🍀 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+function eqArrays(firstArray, secondArray) {
+  let result = true;
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      result = false;
+    }
+  }
+  if (firstArray.length !== secondArray.length) {
+    result = false;
+  }
+  return result;
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [1, 3, 3]), false);
+assertEqual(eqArrays([1, 2, 3], [1, 3]), false);
+assertEqual(eqArrays([], []), true);
+assertEqual(eqArrays([], [1,2]), false);
