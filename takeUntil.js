6491ecd403f8930115ba6cbe5bcb32e1cde3ad55
Declan Davis <dexthefish@gmail.com>
2022-05-05 21:03:57 -0400

    added functions takeUntil and findKey

---
+++ takeUntil.js
@@ -0,0 +1,44 @@
+function eqArrays(firstArray, secondArray) {
+  let result = true;
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      result = false;
+    }
+  }
+  if (firstArray.length !== secondArray.length) {
+    result = false;
+  }
+  return result;
+};
+
+const assertEqual = function(actual,expected) {
+  if (actual === expected) {
+    console.log(`🍀🍀🍀 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+function assertArraysEqual(first, second) {
+  console.log(assertEqual(eqArrays(first, second), true));
+};
+
+
+const takeUntil = function(array, callback) {
+  let result = [];
+  for (const a of array) {
+    if (callback(a)) {
+      break;
+    } else {
+      result.push(a);
+    }
+  }
+  return result;
+}
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+
+assertArraysEqual(takeUntil(data1, x => x < 0), [1,2,5,7,2]);
+console.log('---');
+assertArraysEqual(takeUntil(data2, x => x === ','), ['I\'ve', 'been', 'to', 'Hollywood']);
