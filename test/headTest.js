d73597fe1aefa946e6f40bbd341b58ee476826d8
Declan Davis <dexthefish@gmail.com>
2022-05-10 01:51:40 -0400

    refactored headTest with mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,14 @@
-const head = require('../head');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
+const head   = require('../head');
 
-assertEqual(head([1, 2, 3]), head([1, 4, 5]));
-assertEqual(head([1, 2, 3]), head([4, 5, 6, 7]));
-assertEqual(head(['a', 2, 3]), head(['a', 'c']));
-assertEqual(head([]), head([]));
-assertEqual(head([]), head([1, 2]));
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+  it("returns undefined when given an empty array", () => {
+    assert.strictEqual(head([]), undefined);
+  })
+});
