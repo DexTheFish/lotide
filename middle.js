ca50483bc5af74c5ce26eeda9e88cf86e45920da
Declan Davis <dexthefish@gmail.com>
2022-06-01 13:54:16 -0400

    refactored. Added more descriptive variable names to improve readability. Reorganized logic.

--- middle.js
+++ middle.js
@@ -1,11 +1,16 @@
 const middle = function(array) {
-  let l = array.length;
-  if (l <= 2) {
-    return [];
-  } else if (l % 2 === 0) {
-    return [array[l / 2 - 1], array[l / 2]];
-  } else if (l % 2 === 1) {
-    return [array[Math.floor(l / 2)]];
+  // return a new array containing the middle of array.
+  // if the length is less than 3, the middle is empty.
+  // if the length is even, the middle is a pair of elements.
+  // if the length is odd, the middle is a single element.
+  let length = array.length;
+  let middle = [];
+  if (length % 2 === 0 && length > 2) {
+    middle = [array[length / 2 - 1], array[length / 2]];
   }
+  if (length % 2 === 1 && length > 2) {
+    middle = [array[Math.floor(length / 2)]];
+  }
+  return middle;
 };
 module.exports = middle;
