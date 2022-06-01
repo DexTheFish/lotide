365634f3a25d9f23775a43d1faef11078dd2dfc1
Declan Davis <dexthefish@gmail.com>
2022-05-31 20:14:35 -0400

    refactored the second if-condition in eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -2,7 +2,7 @@ const eqArrays = function(firstArray, secondArray) {
   if (firstArray.length !== secondArray.length) {
     return false;
   }
-  if (firstArray.length > 0) {
+  if (firstArray.length) {
     for (let i = 0; i < firstArray.length; i++) {
       if (firstArray[i] !== secondArray[i]) {
         return false;
