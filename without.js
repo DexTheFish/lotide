7b7eb0f6c8239e25576bdc96a9159b9894dd3fe3
Declan Davis <dexthefish@gmail.com>
2022-06-01 13:53:46 -0400

    refactored to improve runtime. Reviewed with a mentor to confirm that the strategy is high quality.

--- without.js
+++ without.js
@@ -1,4 +1,7 @@
-function without(array, itemsToRemove) {
+// *note* we are avoiding .includes() as an exercise.
+// I want to avoid ALL built in methods >:)
+
+const without = function(array, itemsToRemove) {
   // return a subset of array, removing unwanted elements.
   // unwanted elements are specified by itemsToRemove.
   let newArray = [];
@@ -7,6 +10,7 @@ function without(array, itemsToRemove) {
     for (const remove of itemsToRemove) {
       if (remove === item) {
         keepItem = false;
+        break; // improves runtime
       }
     }
     if (keepItem) {
