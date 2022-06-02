871a3c90e272431b1468799d0fcc046d37468dc9
Declan Davis <dexthefish@gmail.com>
2022-06-02 18:38:29 -0400

    refactoring. Removed unreachable/redundant break statement.

--- findKey.js
+++ findKey.js
@@ -2,8 +2,8 @@ const findKey = function(object, callback) {
   for (const key in object) {
     if (callback(object[key])) {
       return key;
-      break;
     }
+    console.log(key);
   }
 };
 module.exports = findKey;
