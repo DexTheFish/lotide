5b36da324a6552fc9dd306fbb6259b1332256560
Declan Davis <dexthefish@gmail.com>
2022-06-07 00:49:04 -0400

    updated countOnlyTest to use Mocha and Chai

--- test/countOnlyTest.js
+++ test/countOnlyTest.js
@@ -1,25 +1,29 @@
-// these tests should be upgraded to use chai
+const { assert } = require("chai");
+const countOnly = require("../countOnly");
 
-
-/*
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-assertEqual(result1["Jason"], 1);
-assertEqual(result1["Karima"], undefined);
-assertEqual(result1["Fang"], 2);
-assertEqual(result1["Agouhanna"], undefined);
-console.log(result1)
-
-*/
+describe("#countOnly", () => {
+  const firstNames = [
+    "Karl",
+    "Salima",
+    "Agouhanna",
+    "Fang",
+    "Kavith",
+    "Jason",
+    "Salima",
+    "Fang",
+    "Joe"
+  ];
+  const result = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+  it("should not count items that aren't specified", () => {
+    assert.equal(result.Agouhanna, undefined);
+  });
+  it("should properly count an item that appears once", () => {
+    assert.equal(result.Jason, 1);
+  });
+  it("should properly count an item that appears several times", () => {
+    assert.equal(result.Fang, 2);
+  });
+  it("should not count items that appear zero times", () => {
+    assert.equal(result.Karima, undefined);
+  });
+});
